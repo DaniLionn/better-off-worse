c8634a107aa45be981248443d0b88e416f44aa8e
Dani <107291896+DaniLionn@users.noreply.github.com>
2024-05-30 15:48:48 -0600

    change how pfps are made

--- index.js
+++ index.js
@@ -1,39 +1,16 @@
-let messages = [];
-let pfps = undefined;
-fetch("https://danilionn.github.io/danis-bot-website/assets/pfps.json")
-  .then((response) => response.json())
-  .then((json) => {
-    pfps = json;
-  });
+const bpm = 145;
+var tick = 60_000 / bpm;
+const messageLimit = 12;
 
-//console.log(pfps);
-let letters = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
+let messages = [];
+let video, subCount, viewers;
+
+const gravatarTypes = [
+  "identicon",
+  "monsterid",
+  "wavatar",
+  "retro",
+  "robohash",
 ];
 
 const abbrNum = (number, decPlaces) => {
@@ -61,12 +38,28 @@ const abbrNum = (number, decPlaces) => {
   return number;
 };
 
+const getSHA256Hash = async (input) => {
+  const textAsBuffer = new TextEncoder().encode(input);
+  const hashBuffer = await window.crypto.subtle.digest("SHA-256", textAsBuffer);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hash = hashArray
+    .map((item) => item.toString(16).padStart(2, "0"))
+    .join("");
+  return hash;
+};
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 async function randomString(maxChars) {
+  const letters = "abcdefghijklmnopqrstuvwxyz";
+
   let string = "";
 
   let count = 0;
   let max = Math.floor(Math.random() * maxChars) + 1;
-  console.log(maxChars, max);
+
   while (count < max) {
     let newLetter = letters[Math.floor(Math.random() * letters.length)];
 
@@ -80,74 +73,106 @@ async function randomString(maxChars) {
 
   return string;
 }
-function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
 
-async function createMessage() {
-  let list = document.getElementById("chat");
+async function mainLoop() {
+  async function createMessage() {
+    function waitForImage(imgElem) {
+      return new Promise((res) => {
+        if (imgElem.complete) {
+          return res();
+        }
+        imgElem.onload = () => res();
+        imgElem.onerror = () => res();
+      });
+    }
 
-  let message = document.createElement("li");
-  let image = document.createElement("img");
-  let font1 = document.createElement("span");
-  let font2 = document.createElement("span");
+    async function cycleNumber(number, min, max) {
+      var range = max - min + 1;
 
-  image.src = "assets/pfps/" + pfps[Math.floor(Math.random() * pfps.length)];
-  image.width = 32;
-  image.height = 32;
-  image.style = "border-radius: 50%";
-  font1.style = "color:grey; font-family: Roboto, Arial, sans-serif;";
+      var result = ((((number - min) % range) + range) % range) + min;
 
-  font1.id = "text1";
+      if (result > max) {
+        result = result - max;
+      }
 
-  font2.id = "text2";
-  font2.style = "margin-left: 15px; font-family: Roboto, Arial, sans-serif;";
-  font1.innerText = await randomString(7);
-  font2.innerText = "     " + (await randomString(14));
-  message.appendChild(image);
-  message.appendChild(font1);
-  message.appendChild(font2);
+      if (result < min) {
+        result = min;
+      }
 
-  list.appendChild(message);
-  messages.unshift(message);
+      return result;
+    }
+
+    let list = document.getElementById("chat");
+
+    let message = document.createElement("li");
+    let image = document.createElement("img");
+    let font1 = document.createElement("span");
+    let font2 = document.createElement("span");
+
+    let user = await randomString(7);
+    let msg = await randomString(14);
+
+    let hash = await getSHA256Hash(user + msg);
+    let index = await cycleNumber(msg.length, 0, gravatarTypes.length - 1);
+    image.src =
+      "https://gravatar.com/avatar/" +
+      hash +
+      "?s=200&r=pg&d=" +
+      gravatarTypes[index];
+    image.width = 32;
+    image.height = 32;
+    image.style = "border-radius: 50%";
+    font1.style = "color:grey; font-family: Roboto, Arial, sans-serif;";
+
+    font1.id = "text1";
+
+    font2.id = "text2";
+    font2.style = "margin-left: 15px; font-family: Roboto, Arial, sans-serif;";
+    font1.innerText = user;
+    font2.innerText = "     " + msg;
+    message.appendChild(image);
+    message.appendChild(font1);
+    message.appendChild(font2);
+
+    let start = Date.now();
+
+    await waitForImage(image);
+    tick = 60_000 / bpm - (Date.now() - start);
+    list.appendChild(message);
+    messages.unshift(message);
+  }
+
+  if (!video.paused && video.currentTime >= 30) {
+    await createMessage();
+
+    for (let i = 0; i < messages.length; i++) {
+      const message = messages[i];
+      let c = message.children;
+
+      c[1].innerText = await randomString(7);
+      c[2].innerText = await randomString(14);
+    }
+
+    viewers.innerText =
+      numberWithCommas(Math.floor(Math.random() * 850_000)) +
+      " watching now • Started streaming August 25, 2010";
+
+    subCount.innerText =
+      abbrNum(Math.floor(Math.random() * 850_000), 1) + " subscribers";
+  }
+
+  if (messages.length > messageLimit) {
+    if (messages[messageLimit + 1]) {
+      messages[messageLimit + 1].remove();
+      messages.splice(messageLimit + 1, 1);
+    }
+  }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  const video = document.getElementById("videoPlayer");
+  video = document.getElementById("videoPlayer");
+  subCount = document.getElementById("subs");
+  viewers = document.getElementById("viewers");
 
-  setInterval(() => {
-    if (!video.paused && video.currentTime > 29) {
-      createMessage();
-    }
-  }, 414);
-
-  setInterval(async () => {
-    if (messages.length > 14) {
-      if (messages[15]) {
-        messages[15].remove();
-        messages.splice(15, 1);
-      }
-    }
-  }, 414);
-
-  setInterval(async () => {
-    if (!video.paused && video.currentTime > 29) {
-      for (let index = 0; index < messages.length; index++) {
-        const message = messages[index];
-        let c = message.children;
-
-        // console.log(c);
-
-        c[1].innerText = await randomString(7);
-        c[2].innerText = await randomString(14);
-      }
-
-      document.getElementById("viewers").innerText =
-        numberWithCommas(Math.floor(Math.random() * 850_000)) +
-        " watching now • Started streaming August 25, 2010";
-
-      document.getElementById("subs").innerText =
-        abbrNum(Math.floor(Math.random() * 850_000), 1) + " subscribers";
-    }
-  }, 414);
+  setInterval(mainLoop, tick);
 });
